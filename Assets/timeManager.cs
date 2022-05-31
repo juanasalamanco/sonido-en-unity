9bdf552d8fb0866b45b2b39b284b812af437913a
juanasalamanco <juanasalamanco@gmail.com>
2022-05-31 13:25:27 -0300

    update

--- Assets/timeManager.cs
+++ Assets/timeManager.cs
@@ -7,11 +7,16 @@ public class timeManager : MonoBehaviour
 {
     public Text txtTime;
     public Text txtTimeFloored;
+    public Text txtcustomtext;
+
+    float customtime;
+    bool iscounting;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        customtime = 0;
+        iscounting = false;
     }
 
     // Update is called once per frame
@@ -22,5 +27,21 @@ public class timeManager : MonoBehaviour
         txtTime.text = time.ToString();
 
         txtTimeFloored.text = Mathf.Floor(time).ToString();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            iscounting = true;
+        }
+        if (iscounting)
+        {
+            customtime += Time.deltaTime;
+        }
+
+        txtcustomtext.text = customtime.ToString();
+
+        if(customtime > 10)
+        {
+            //perdiste
+        }
     }
 }
